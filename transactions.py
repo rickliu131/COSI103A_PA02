312b2af57da5c1245b9d1b35d5473cadb0664a98
siyuyang2001 <siyuyang@brandeis.edu>
2022-03-23 13:41:14 -0400

    transaction.py

---
+++ transactions.py
@@ -0,0 +1,9 @@
+
+
+class Transaction():
+    '''
+    Transaction class that will store financial transactions with the fields
+    '''
+    def __init__(self,filename=()):
+        ''' courses is a tuple of the courses being offered '''
+        self.filename = filename
